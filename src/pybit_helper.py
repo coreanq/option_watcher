e5a5e64db5be2faeaafd238b211c46ff2f9b0f92
Charles Park <coreanq@gmail.com>
2023-09-27 17:25:37 +0900

    place batch order tesing

--- src/pybit_helper.py
+++ src/pybit_helper.py
@@ -55,7 +55,13 @@ for symbol_name in symbol_list:
         jango_info[symbol_name]['b'] = result['b']
         jango_info[symbol_name]['a'] = result['a']
 
-        current_price = float(result['b'][0][0])
+
+        current_price = 0 
+        if( len(result['b']) != 0):
+            current_price = float(result['b'][0][0])
+        else:
+            current_price = 0
+
         current_size = float(jango_info[symbol_name]['size'])
 
         original_price = float( jango_info[symbol_name]['avgPrice'] ) 
@@ -63,7 +69,7 @@ for symbol_name in symbol_list:
 
         jango_info[symbol_name]['profit'] = current_price * current_size  - original_price * current_size  - fee
 
-        print( '{} profit: {} $'.format( symbol_name, jango_info[symbol_name]['profit'] ) )
+        print( '{} profit: {} $'.format( symbol_name, round( jango_info[symbol_name]['profit'] , 2) ) )
 
 
 total_profit = 0
@@ -76,6 +82,39 @@ for key, value in jango_info.items():
 print( 'total profit {}'.format( total_profit))
 
 
+
+# batch order
+symbol_name1 = 'ETH-28SEP23-1550-P'
+symbol_name2 = 'ETH-28SEP23-1625-C'
+
+result = session.place_batch_order(
+    category="option",
+    request=[
+        {
+            "category": "option",
+            "symbol": symbol_name1,
+            "orderType": "Limit",
+            "side": "Sell",
+            "qty": "0.1",
+            "price": "1.2",
+            "orderLinkId": "option-test-001",
+            "mmp": False,
+            "reduceOnly": True # for option closing side Sell and must reduceOnly true 
+        },
+        {
+            "category": "option",
+            "symbol": symbol_name2, 
+            "orderType": "Limit",
+            "side": "Sell",
+            "qty": "0.1",
+            "price": "1.0",
+            "orderLinkId": "option-test-002",
+            "mmp": False,
+            "reduceOnly": True # for option closing side Sell and must reduceOnly true 
+        }
+    ]
+)
+print( json.dumps(result, indent=2)  )
 
 #  최근  거래  내역 (  내 거래 내역 아님 )
 # print(session.get_public_trade_history(
